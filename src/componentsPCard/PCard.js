29f310874a83212b9fd437df7cccf08d70820638
santiago <santiago.scheffknecht@student.htldornbirn.at>
2024-01-19 14:21:36 +0100

    added coponents

---
+++ src/componentsPCard/PCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './PCard.css'
+export default function PCard() {
+    return (
+        <div class="PCard">
+            <h1>{name.card}</h1>
+        </div>
+    )
+}
