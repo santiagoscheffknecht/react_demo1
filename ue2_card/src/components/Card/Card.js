a711b5fabb5c9776f6523acc6062e8b5fc908bad
santiago <santiago.scheffknecht@student.htldornbirn.at>
2024-02-21 12:18:58 +0100

    Weieter gearbeitet

--- ue2_card/src/components/Card/Card.js
+++ ue2_card/src/components/Card/Card.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import '.PCar.module.css'
+import styles from './PCard.module.css'
 export default function Card(info) {
     return (
-        <div>
-            <div>
+        <div className={styles.PCard}>
+            <div className='bg'>
                 <h1>{info.name}</h1>
                 <h2>{info.title}</h2>
             </div>
 
             <div>
-                <img scr={info.imageUri} alt='' />
+                <img src={info.imageUri} alt='' />
             </div>
         </div>
     )
