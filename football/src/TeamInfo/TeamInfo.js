6290c9fe0849c8c2d2b1e6efdf53e129953fbde0
santiago <santiago.scheffknecht@student.htldornbirn.at>
2024-03-27 23:16:37 +0100

    id hinzugefügt

--- football/src/TeamInfo/TeamInfo.js
+++ football/src/TeamInfo/TeamInfo.js
@@ -7,8 +7,9 @@ export default function StarSite() {
     return (
         <div style={{ backgroundImage: `url(${bottomImage})` }} className={style.backgroundImage}> {/* Setze das Hintergrundbild */}
             <div className={style.top}>
-                <h1 className={style.letter}>Austrian Bundesliga</h1>
+                <h1 className={style.letter}>teamName</h1>
             </div>
+
         </div>
     );
 }
